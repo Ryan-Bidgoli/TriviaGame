0acfe33cc27f8886bc0a863ac021160c84669cf7
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-06 23:46:38 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,33 @@ var intervalId;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
+
+var questions = [{
+    question: "Which album sold more records?",
+    answerList: ["Alanis Morissette's \"Jagged Little Pill\"", "Spice Girls' \"Spice\"", "Shania Twain's \"Come On Over\"", "Metallica's \"Metallica\""],
+    answer: 0
+},{
+    question: 
+    answerList:  
+    answer: 
+},{
+    question:  
+    answerList: 
+    answer: 
+},{
+    question: 
+    answerList:  
+    answer:  
+},{
+    question: 
+    answerList:  
+    answer:  
+},{
+    question:  
+    answerList:  
+    answer: 
+},{
+    question:  
+    answerList: 
+    answer: 
+}];
