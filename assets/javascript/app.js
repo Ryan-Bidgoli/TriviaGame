69666731081220fa376a4ecc7289182fcd980037
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-06 23:58:51 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,13 +11,13 @@ var questions = [{
     answerList: ["Alanis Morissette's \"Jagged Little Pill\"", "Spice Girls' \"Spice\"", "Shania Twain's \"Come On Over\"", "Metallica's \"Metallica\""],
     answer: 0
 },{
-     question: "Who sang the \"Friends\" theme song?",
+    question: "Who sang the \"Friends\" theme song?",
     answerList: ["Spin Doctors", "The Wallflowers", "Gin Blossoms", "The Rembrandts"],
     answer: 3
 },{
-    question:  
-    answerList: 
-    answer: 
+    question: "The 1996 Summer Olympics where held in which U.S. city?",
+    answerList: ["San Antonio", "Atlanta", "Denver", "Houston"],
+    answer: 1
 },{
     question: 
     answerList:  
