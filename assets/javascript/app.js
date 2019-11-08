98c5eae567d11c85e12c2e1a58a21ab8e6cff36c
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-07 19:15:22 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 var time;
-
 var intervalId;
-
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
@@ -15,23 +13,23 @@ var questions = [{
     answerList: ["Spin Doctors", "The Wallflowers", "Gin Blossoms", "The Rembrandts"],
     answer: 3
 },{
+     question: "What was the flavor of the soft drink Surge?",
+    answerList: ["Lemon", "Lime", "Tropical", "Citrus"],
+    answer: 3
+},{
+    question: "Which fruit contains the most protein per 100 calories?", 
+    answerList: ["Guava", "Avocado", "Banana", "Blackberries" ],
+		answer: 0
+},{
     question: "The 1996 Summer Olympics where held in which U.S. city?",
     answerList: ["San Antonio", "Atlanta", "Denver", "Houston"],
     answer: 1
 },{
-    question: 
-    answerList:  
-    answer:  
+    question: "What is the name of the high school in \"Boy Meets World\"?",
+    answerList: ["John Adams High School", "John Henry High School", "John Hancock High School", "Johnny Appleseed High School"],
+    answer: 0
 },{
-    question: 
-    answerList:  
-    answer:  
-},{
-    question:  
-    answerList:  
-    answer: 
-},{
-    question:  
-    answerList: 
-    answer: 
+    question: "When the iBook was released in 1999 which was NOT one of the five colors available?",
+    answerList: ["Tangerine", "Blueberry", "Key Lime", "Watermelon"],
+    answer: 3
 }];
